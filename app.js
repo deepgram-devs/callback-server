da256a17fbdd98d28c041475a6f6fd83daecfc29
Sandra Rodgers <sandra.rodgers@deepgram.com>
2023-11-22 19:29:14 -0600

    remove google

--- app.js
+++ app.js
@@ -2,59 +2,30 @@ const express = require("express");
 const app = express();
 app.use(express.json());
 
-const { google } = require('googleapis');
-const fs = require('fs');
-const authenticate = require('./auth');
-
-// Your file-specific configurations
-const credentialsPath = 'credentials.json';
-const tokenPath = 'token.json';
-const scopes = ['https://www.googleapis.com/auth/drive'];
+let transcription = "";
 
 // Route handler for webhook
 app.post("/hook", (req, res) => {
   console.log("File sent to server.");
-  console.log(req.body); // See the data
+  console.log("req.body", req.body);
+  console.log("req.body.results", req.body.results);
+  if (
+    req.body &&
+    req.body.results &&
+    req.body.results.channels &&
+    req.body.results.channels[0].alternatives
+  ) {
+    transcription += req.body.results.channels[0].alternatives[0].transcript;
+    console.log(transcription);
+  }
 
-  // Authenticate and execute Google Drive API functions
-  authenticate(credentialsPath, tokenPath, scopes, (err, oAuth2Client) => {
-    if (err) {
-      console.error('Authentication error:', err);
-      return res.status(500).send("Error authenticating");
-    }
+  res.send(req.body);
+});
 
-    const drive = google.drive({ version: 'v3', auth: oAuth2Client });
-
-    // Specify the file ID of the file you want to share
-    const fileId = req.body.fileId;
-
-    if (!fileId) {
-      console.error("File ID not found in the payload.");
-      return res.status(400).send("File ID not found in the payload.");
-    }
-
-    // Set the permission type to 'anyone' and the role to 'reader' (adjust as needed)
-    const permission = {
-      type: 'anyone',
-      role: 'reader',
-    };
-
-    // Create a shareable link
-    drive.permissions.create({
-      fileId: fileId,
-      requestBody: permission,
-      fields: 'id',
-    })
-      .then(response => {
-        const link = `https://drive.google.com/uc?id=${fileId}`;
-        console.log(`Shareable link: ${link}`);
-        res.status(200).send(link); // Return empty response to Deepgram
-      })
-      .catch(error => {
-        console.error(error.message);
-        res.status(500).send("Error processing the file");
-      });
-  });
+app.get("/transcription", (req, res) => {
+  console.log("getting transcription", transcription);
+  res.json(transcription);
+  transcription = "";
 });
 
 // Start express server
