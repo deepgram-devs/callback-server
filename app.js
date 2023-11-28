4ed287c78f958ec97b674ef8afba8f145621fd92
Sandra Rodgers <sandra.rodgers@deepgram.com>
2023-11-28 16:14:57 -0600

    update server

--- app.js
+++ app.js
@@ -7,25 +7,33 @@ let transcription = "";
 // Route handler for webhook
 app.post("/hook", (req, res) => {
   console.log("File sent to server.");
-  console.log("req.body", req.body);
-  console.log("req.body.results", req.body.results);
+  console.log(req.body);
+  transcription = "";
+
   if (
-    req.body &&
     req.body.results &&
     req.body.results.channels &&
-    req.body.results.channels[0].alternatives
+    req.body.results.channels[0] &&
+    req.body.results.channels[0].alternatives &&
+    req.body.results.channels[0].alternatives[0] &&
+    req.body.results.channels[0].alternatives[0].transcript
   ) {
-    transcription += req.body.results.channels[0].alternatives[0].transcript;
-    console.log(transcription);
+    transcription = req.body.results.channels[0].alternatives[0].transcript;
+    console.log("transcription", transcription);
+  } else {
+    transcription = "No transcription available.";
+    console.log("No transcription available.");
   }
-
-  res.send(req.body);
 });
 
 app.get("/transcription", (req, res) => {
   console.log("getting transcription", transcription);
-  res.json(transcription);
-  transcription = "";
+  if (transcription.length > 0) {
+    res.json(transcription);
+    transcription = "";
+  } else {
+    res.json("No transcription available.");
+  }
 });
 
 // Start express server
